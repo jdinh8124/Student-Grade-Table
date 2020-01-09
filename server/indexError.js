480c2356e141941c3028c08ae7328c38bc488ab2
James <mrjames8124@gmail.com>
2020-01-08 19:41:07 -0800

    working through error handling

--- server/indexError.js
+++ server/indexError.js
@@ -1,8 +1,8 @@
-// export default function clientUnexpectedError(err, req, res, next) {
-//   if (err) {
-//     const notFound = { error: `ID ${req} does not exsist` };
-//     res.status(404).json(notFound);
-//   } else {
-//     next(err);
-//   }
-// }
+export default function clientUnexpectedError(err, req, res, next) {
+  if (err) {
+    const notFound = { error: `ID ${req} does not exsist` };
+    res.status(404).json(notFound);
+  } else {
+    next(err);
+  }
+}
