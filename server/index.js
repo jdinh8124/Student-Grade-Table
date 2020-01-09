f33f9124a8fa9b0804f61bb942fec44aca2e62ba
James <mrjames8124@gmail.com>
2020-01-09 09:57:06 -0800

    finished custom error handling

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const data = require('../database/data.json');
 const fs = require('fs');
 const app = express();
-// const Errors = require('./indexError');
+const Errors = require('./indexError');
 
 const dbPath = path.resolve(__dirname, '../database/data.json');
 const filePath = express.static(path.join(__dirname, '/public'));
@@ -17,9 +17,8 @@ app.get('/api/grades/', (req, res) => {
 });
 
 app.post('/api/grades/', (req, res) => {
-  if (!req.body) {
-    const errorObjectNoBody = { error: 'There was no content to POST' };
-    res.status(400).json(errorObjectNoBody);
+  if (!req.body.name || !req.body.course || !req.body.grade) {
+    Errors(400, null, res);
   } else {
     const objToPush = {
       name: req.body.name,
@@ -32,8 +31,7 @@ app.post('/api/grades/', (req, res) => {
     data.nextId++;
     fs.writeFile(dbPath, jsonSend, 'utf-8', function (err) {
       if (err) {
-        const genericError = { error: 'An unexpected error occurred.' };
-        res.status(500).json(genericError);
+        Errors(500, null, res);
       } else {
         res.status(201).json(objToPush);
 
@@ -45,8 +43,7 @@ app.post('/api/grades/', (req, res) => {
 app.delete('/api/grades/:id', (req, res) => {
   const parsedId = parseInt(req.params.id);
   if (isNaN(parsedId) || parsedId < 0) {
-    const errorObject = { error: 'ID must be a positive integer' };
-    res.status(400).json(errorObject);
+    Errors(404, parsedId, res);
   } else {
     const found = data.grades.findIndex(element => element.id === parsedId);
     if (found >= 0) {
@@ -54,27 +51,23 @@ app.delete('/api/grades/:id', (req, res) => {
       const jsonSend = JSON.stringify(data, null, 2);
       fs.writeFile(dbPath, jsonSend, 'utf-8', function (err) {
         if (err) {
-          const genericError = { error: 'An unexpected error occurred.' };
-          res.status(500).json(genericError);
+          Errors(500, null, res);
         } else {
           res.status(204).json();
         }
       });
     } else {
-      const notFound = { error: `ID ${parsedId} does not exsist` };
-      res.status(404).json(notFound);
+      Errors(404, parsedId, res);
     }
   }
 });
 
 app.put('/api/grades/:id', (req, res) => {
   const parsedId = parseInt(req.params.id);
-  if (!req.body) {
-    const errorObjectNoBody = { error: 'There was no content to PUT' };
-    res.status(400).json(errorObjectNoBody);
+  if (!req.body.name || !req.body.course || !req.body.grade) {
+    Errors(400, null, res);
   } else if (isNaN(parsedId) || parsedId < 0) {
-    const errorObject = { error: 'ID must be a positive integer' };
-    res.status(400).json(errorObject);
+    Errors(404, parsedId, res);
   } else {
     const foundIndex = data.grades.findIndex(element => element.id === parsedId);
     if (foundIndex >= 0) {
@@ -88,15 +81,13 @@ app.put('/api/grades/:id', (req, res) => {
       const jsonSend = JSON.stringify(data, null, 2);
       fs.writeFile(dbPath, jsonSend, 'utf-8', function (err) {
         if (err) {
-          const genericError = { error: 'An unexpected error occurred.' };
-          res.status(500).json(genericError);
+          Errors(500, null, res);
         } else {
           res.status(201).json(objToPush);
         }
       });
     } else {
-      const notFound = { error: `ID ${parsedId} does not exsist` };
-      res.status(404).json(notFound);
+      Errors(404, parsedId, res);
     }
   }
 });
