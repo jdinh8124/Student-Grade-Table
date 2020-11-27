5445402542efab0a637af3a5bbcb49f246e01c45
James Dinh <jdinh8124@gmail.com>
2020-11-27 12:13:18 -0800

    adjusted margin left for average grade

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = props => {
   return (
-    <div className="row container-fluid  justify-content-center text-center mb-sm-4 mb-xs-4 pb-3 ">
+    <div className="row container-fluid  justify-content-center text-center mb-4 pb-3 ">
       <div className="col-xl-4 col-lg-9">
         <div className="h1 titleFont">{props.text}</div>
       </div>
-      <div className="col-xl-3 offset-xl-5 col-lg-6 container-fluid   ">
+      <div className="ml-xl-auto">
         <div className="h2 d-inline fontInBetween pr-1">Average Grade</div>
         <span className="badge d-inline fontInBetween border-2 rounded badge-secondary">{props.grade}</span>
       </div>
